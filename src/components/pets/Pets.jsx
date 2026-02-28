626ebabf5bce95f38f727728cb037281717957d7
sergey123423213 <grinya.0895@gmail.com>
2026-02-28 17:27:28 +0200

    Refactor Pets component and update image imports
    Refactored Pets component to improve structure and readability. Updated image imports and fixed repeated content.

--- src/components/pets/Pets.jsx
+++ src/components/pets/Pets.jsx
@@ -1,29 +1,32 @@
-import { useEffect, useRef, useState } from 'react';
 import './Pets.css';
 import ghost from '../../accets/pets/gostdog.png'
 import dogwball from '../../accets/pets/dogwball.png'
-import dogwithball from '../../pets/future/dogwithball.png'
-import cattalking from '../../pets/future/cattalking.png'
+import dogwithball from '../../accets/pets/dogwithball.png'
+import cattalking from '../../accets/pets/cattalking.png'
+
 function Pets() {
-    return(
+    return (
         <div className='pets'>
             <p className="pets-title">Interacting with our pets</p>
-            <div className="pets-news">
-                <img src="{ghost}" className='pets-news-picutre' alt="" />
-                <p className="pets-news-headline">Rescue pups pose as ghosts in festive photo shoot</p>
+            <div className="pets-box">
+                <div className="pets-news">
+                    <img src={ghost} className='pets-news-picutre' alt="" />
+                    <p className="pets-news-headline">Rescue pups pose as ghosts in festive photo shoot</p>
+                </div>
+                <div className="pets-news">
+                    <img src={dogwball} className='pets-news-picutre' alt="" />
+                    <p className="pets-news-headline">Rescuew pups pose as ghosts in festive photo shoot</p>
+                </div>
+                <div className="pets-news">
+                    <img src={dogwithball} className='pets-news-picutre' alt="" />
+                    <p className="pets-news-headline">Rescuew pups pose as ghosts in festive photo shoot</p>
+                </div>
+                <div className="pets-news">
+                    <img src={cattalking} className='pets-news-picutre' alt="" />
+                    <p className="pets-news-headline">Rescuew pups pose as ghosts in festive photo shoot</p>
+                </div>
             </div>
-            <div className="pets-news">
-                <img src="{dogwball}" className='pets-news-picutre' alt="" />
-                <p className="pets-news-headline">Rescuew pups pose as ghosts in festive photo shoot</p>
-            </div>
-            <div className="pets-news">
-                <img src="{dogwithball}" className='pets-news-picutre' alt="" />
-                <p className="pets-news-headline">Rescuew pups pose as ghosts in festive photo shoot</p>
-            </div>
-            <div className="pets-news">
-                <img src="{cattalking}" className='pets-news-picutre' alt="" />
-                <p className="pets-news-headline">Rescuew pups pose as ghosts in festive photo shoot</p>
-            </div>
+            <button className="pets-more">See more</button>
         </div>
     )
 }
